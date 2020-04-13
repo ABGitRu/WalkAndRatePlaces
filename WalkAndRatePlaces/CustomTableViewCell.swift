4d3473a97a69d88c0aa6a243dc97468f6806a2dc
Alexey Barinov <begoodbebad2012@gmail.com>
2020-04-13 12:52:09 +0300

    added Custom tablebiewcell

---
+++ WalkAndRatePlaces/CustomTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTableViewCell.swift
+//  WalkAndRatePlaces
+//
+//  Created by Mac on 13.04.2020.
+//  Copyright © 2020 Alexey Barinov. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+    
+    @IBOutlet var locationLabel: UILabel!
+    
+    @IBOutlet var typeLabel: UILabel!
+    
+}
